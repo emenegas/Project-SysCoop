1f85b29741aa2ec77181a0ea41957c040204b818
emenegas <42917411+emenegas@users.noreply.github.com>
2018-12-05 22:34:51 -0200

    ajuste

--- SYSCOOP/application/views/Login_view.php
+++ SYSCOOP/application/views/Login_view.php
@@ -12,7 +12,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
   <link rel="shortcut icon" href="assets/planta1.ico" >
-  <link rel="stylesheet" type="text/css" href="assets/style.css">
+  <link rel="stylesheet" type="text/css" href="assets/styleLogin.css">
 </head>
 <body>
   <?php if(isset($formerror)): ?>
@@ -63,94 +63,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
+
 </html>
-<style type="text/css">
 
-html,body{
-  background-image: url('assets/campo.jpg');
-  background-size: cover;
-  background-repeat: no-repeat;
-  height: 100%;
-}
+<!-- <style type="text/css">
 
-.container{
-  height: 100%;
-  align-content: center;
-}
 
-.card{
-  height: 300px;
-  margin-top: auto;
-  margin-bottom: auto;
-  width: 400px;
-  background-color: rgba(0,0,0,0.5) !important;
-}
-
-.social_icon span{
-  font-size: 60px;
-  margin-left: 10px;
-  color: #FFC312;
-}
-
-.social_icon span:hover{
-  color: white;
-  cursor: pointer;
-}
-
-.card-header h3{
-  color: white;
-}
-
-.social_icon{
-  position: absolute;
-  right: 20px;
-  top: -45px;
-}
-
-.input-group-prepend span{
-  width: 50px;
-  background-color: #FFC312;
-  color: black;
-  border:0 !important;
-}
-
-input:focus{
-  outline: 0 0 0 0  !important;
-  box-shadow: 0 0 0 0 !important;
-
-}
-
-.remember{
-  color: white;
-}
-
-.remember input
-{
-  width: 20px;
-  height: 20px;
-  margin-left: 15px;
-  margin-right: 5px;
-}
-
-.login_btn{
-  color: black;
-  background-color: #FFC312;
-  width: 100px;
-}
-
-.login_btn:hover{
-  color: black;
-  background-color: white;
-}
-
-.links{
-  color: white;
-}
-
-.links a{
-  margin-left: 4px;
-}
-</style>
+</style> -->
 <script language=javascript>
   <!--
     function fone(obj,prox) {
